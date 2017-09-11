9440380518b75102343612e28c6e1757e892bb8d
developertqw2017 <2144799613@qq.com>
2017-09-11 12:38:20 +0800

    v1.5.6 界面修改

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
                         </div>
                       </div>
                     </div>
-                    <div class="text"> <img width="" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1505109081280&di=d4bfc441042bfc73364f86af9ca1601a&imgtype=0&src=http%3A%2F%2Fwww.xz7.com%2Fup%2F2017-8%2F2017891713542540.jpg"> </div>
+                    <div class="text"> <img style="max-width:50%" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1505109081280&di=d4bfc441042bfc73364f86af9ca1601a&imgtype=0&src=http%3A%2F%2Fwww.xz7.com%2Fup%2F2017-8%2F2017891713542540.jpg"> </div>
                   </div>
                 </li>
                 <li class="from_user left"> <a href="#" class="avatar"><img width="50px" src="http://ww3.sinaimg.cn/large/0060lm7Tly1fjfgh731jdj30gr0grwlf.jpg"/></a>
@@ -213,24 +213,24 @@
                 <li>
 
                   <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Aug</span> </div>
-                  <div class="news-item-detail"> <a href="http://www.by EGrappler/thursday-roundup-40/" class="news-item-title" target="_blank">Thursday Roundup # 40</a>
-                    <p class="news-item-preview"> This is our web design and development news series where we share our favorite design/development related articles, resources, tutorials and awesome freebies. </p>
+                  <div class="news-item-detail"> <a class="news-item-title" target="_blank">Linux学习之路 # 4</a>
+                    <p class="news-item-preview"> This </p>
                   </div>
 
                 </li>
                 <li>
 
                   <div class="news-item-date"> <span class="news-item-day">15</span> <span class="news-item-month">Jun</span> </div>
-                  <div class="news-item-detail"> <a href="http://www.by EGrappler/retina-ready-responsive-app-landing-page-website-template-app-landing/" class="news-item-title" target="_blank">Retina Ready Responsive App Landing Page Website Template – App Landing</a>
-                    <p class="news-item-preview"> App Landing is a retina ready responsive app landing page website template perfect for software and application developers and small business owners looking to promote their iPhone, iPad, Android Apps and software products.</p>
+                  <div class="news-item-detail"> <a class="news-item-title" target="_blank">TCP/IP相关 #1</a>
+                    <p class="news-item-preview"> App products.</p>
                   </div>
 
                 </li>
                 <li>
 
                   <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Oct</span> </div>
-                  <div class="news-item-detail"> <a href="http://www.by EGrappler/open-source-jquery-php-ajax-contact-form-templates-with-captcha-formify/" class="news-item-title" target="_blank">Open Source jQuery PHP Ajax Contact Form Templates With Captcha: Formify</a>
-                    <p class="news-item-preview"> Formify is a contribution to lessen the pain of creating contact forms. The collection contains six different forms that are commonly used. These open source contact forms can be customized as well to suit the need for your website/application.</p>
+                  <div class="news-item-detail"> <a  class="news-item-title" target="_blank">Redis 简单 #1</a>
+                    <p class="news-item-preview"> Formify </p>
                   </div>
 
                 </li>
